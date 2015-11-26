fd80c4cb5f36e865ef130a0084f5231c5dbf4248
Rotaro <-->
2015-11-26 14:30:49 +0200

    Small improvements to paths.

---
+++ setup.py
@@ -0,0 +1,15 @@
+﻿from distutils.core import setup, Extension
+import os
+
+include_files = [
+ os.path.join(os.getcwd(), "src", "fishery_py_module.c"),
+ os.path.join(os.getcwd(), "src", "fishery_functions.c"),
+ os.path.join(os.getcwd(), "src", "help_functions.c")]
+
+fishery_module = Extension('fishery',include_files, include_dirs=[os.path.join(os.getcwd(), "include")])
+
+setup(name='Fishery Simulation', 
+      version='1.0', 
+      description="Simple simulation of a fishery, i.e. a fish population being fished.",
+      url="https://github.com/Rotaro/Fishery/",
+      ext_modules=[fishery_module])
